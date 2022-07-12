ff814022d34b4e10f5075c0eb8b6107553ae20e0
Italo Souza Melo <italosm29@gmail.com>
2022-07-12 04:10:58 -0300

    add player and routes

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from "phosphor-react";
 import { isPast, format } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
+import { Link } from "react-router-dom";
 
 interface LessonProps {
   slug: string;
@@ -19,33 +20,37 @@ export function Lesson({ slug, title, availableAt, type }: LessonProps) {
     }
   );
   return (
-    <div
-      className={`${
+    <Link
+      to={`/event/lesson/${slug}`}
+      target="_self"
+      className={`group ${
         isLessonAvailable ? "pointer-events-auto" : "pointer-events-none"
-      } `}
+      }`}
     >
-      <a href="#" target="_self">
-        <span className="text-gray-300">{availableDateFormatted}</span>
-        <div className="rounded border border-gray-500 p-4 mt-2 hover:border-green-500">
-          <header className="flex items-center justify-between">
-            {isLessonAvailable ? (
-              <span className="flex items-center gap-2 text-sm text-blue-500 font-medium">
-                <CheckCircle size={20} />
-                Conteúdo liberado
-              </span>
-            ) : (
-              <span className="flex items-center gap-2 text-sm text-orange-500 font-medium">
-                <Lock size={20} />
-                Em breve
-              </span>
-            )}
-            <span className="text-xs rounded px-2 py-[0.124rem] text-white border border-green-500  font-bold">
-              {type === "live" ? "AO VIVO" : "AULA PRÁTICA"}
+      <span className="text-gray-300">{availableDateFormatted}</span>
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
+        <header className="flex items-center justify-between">
+          {isLessonAvailable ? (
+            <span className="flex items-center gap-2 text-sm text-blue-500 font-medium">
+              <CheckCircle size={20} />
+              Conteúdo liberado
             </span>
-          </header>
-          <strong className="text-gray-200 mt-5 block">{title}</strong>
-        </div>
-      </a>
-    </div>
+          ) : (
+            <span className="flex items-center gap-2 text-sm text-orange-500 font-medium">
+              <Lock size={20} />
+              Em breve
+            </span>
+          )}
+          <span
+            className={`text-xs rounded px-2 py-[0.124rem] text-white border  ${
+              isLessonAvailable ? "border-green-500" : "border-orange-500"
+            }  font-bold group-hover:text-green-500`}
+          >
+            {type === "live" ? "AO VIVO" : "AULA PRÁTICA"}
+          </span>
+        </header>
+        <strong className="text-gray-200 mt-5 block">{title}</strong>
+      </div>
+    </Link>
   );
 }
--- src/components/Player.tsx
+++
@@ -1,7 +0,0 @@
-interface PlayerProps {
-  title?: string;
-}
-
-export function Player({ title }: PlayerProps) {
-  return <div className="flex-1">Player</div>;
-}
