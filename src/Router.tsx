8bf7df9d70670609a4229499edb42452d0e7f26d
Italo Souza Melo <italosm29@gmail.com>
2022-07-15 15:17:30 -0300

    add _redirects

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,9 +5,9 @@ import { Subscribe } from "./pages/Subscribe";
 export function Router() {
   return (
     <Routes>
-      <Route index element={<Subscribe />} />
-      <Route path="event/*" element={<Event />} />
-      <Route path="event/lesson/:slug" element={<Event />} />
+      <Route path="/" element={<Subscribe />} />
+      <Route path="/event" element={<Event />} />
+      <Route path="/event/lesson/:slug" element={<Event />} />
     </Routes>
   );
 }
