8413495a33afd6abaf235e72d31d5968e7a03cc0
Italo Souza Melo <italosm29@gmail.com>
2022-07-15 14:17:58 -0300

    React router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { Event } from "./pages/Event";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "./lib/apollo";
 import { Router } from "./Router";
-import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <ApolloProvider client={client}>
-        <Router />
-      </ApolloProvider>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <Router />
+    </ApolloProvider>
   );
 }
 
