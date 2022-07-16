a6bda76f5f3c96e519a15d135141236bf922cbf4
Italo Souza Melo <italosm29@gmail.com>
2022-07-15 21:21:20 -0300

    Button and video configuration

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -8,63 +8,73 @@ export function Sidebar() {
   const [menuVisible, setMenuVisible] = useState(true);
 
   return (
-    <aside
-      className={`
-      bg-gray-700 p-6 border-l border-gray-600
-        md:w-[348px] md:relative
-        ${menuVisible ? "absolute right-0 left-0 w-screen h-screen " : ""}
-      `}
-    >
+    <>
       <button
         onClick={() => {
           setMenuVisible(!menuVisible);
         }}
         className="absolute top-5 right-5 text-white md:hidden md:opacity-0 ease-in-out duration-300"
       >
-        {menuVisible ? <X size={32} /> : <List size={32} />}
+        {!menuVisible ? <List size={32} /> : <X size={32} />}
       </button>
-      {menuVisible ? (
-        <>
-          <span className="text-bold text-2xl pb-6 mb-6 border-b border-gray-500 md:block flex justify-center">
-            Cronograma das aulas
-          </span>
-          <div
-            onClick={() => setMenuVisible(!menuVisible)}
-            className="flex flex-col gap-8"
-          >
-            {data?.lessons.map((lesson) => {
-              return (
-                <Lesson
-                  key={lesson.id}
-                  title={lesson.title}
-                  slug={lesson.slug}
-                  availableAt={new Date(lesson.availableAt)}
-                  type={lesson.lessonType}
-                />
-              );
-            })}
-          </div>
-        </>
-      ) : (
-        <>
-          <span className="md:text-bold md:text-2xl md:pb-6 md:mb-6 md:border-b md:border-gray-500 md:block hidden">
-            Cronograma das aulas
-          </span>
-          <div className="md:flex md:flex-col md:gap-8 hidden">
-            {data?.lessons.map((lesson) => {
-              return (
-                <Lesson
-                  key={lesson.id}
-                  title={lesson.title}
-                  slug={lesson.slug}
-                  availableAt={new Date(lesson.availableAt)}
-                  type={lesson.lessonType}
-                />
-              );
-            })}
-          </div>
-        </>
-      )}
-    </aside>
+      <aside
+        className={`
+      bg-gray-700 p-6 border-l border-gray-600
+        md:w-[348px] md:relative
+        ${menuVisible ? "absolute right-0 left-0 w-screen h-screen " : ""}
+      `}
+      >
+        {/* <button
+          onClick={() => {
+            setMenuVisible(!menuVisible);
+          }}
+          className="absolute top-5 right-5 text-white md:hidden md:opacity-0 ease-in-out duration-300"
+        >
+          {menuVisible ? <X size={32} /> : null}
+        </button> */}
+        {menuVisible ? (
+          <>
+            <span className="text-bold text-2xl pb-6 mb-6 border-b border-gray-500 md:block flex justify-center">
+              Cronograma das aulas
+            </span>
+            <div
+              onClick={() => setMenuVisible(!menuVisible)}
+              className="flex flex-col gap-8"
+            >
+              {data?.lessons.map((lesson) => {
+                return (
+                  <Lesson
+                    key={lesson.id}
+                    title={lesson.title}
+                    slug={lesson.slug}
+                    availableAt={new Date(lesson.availableAt)}
+                    type={lesson.lessonType}
+                  />
+                );
+              })}
+            </div>
+          </>
+        ) : (
+          <>
+            <span className="md:text-bold md:text-2xl md:pb-6 md:mb-6 md:border-b md:border-gray-500 md:block hidden">
+              Cronograma das aulas
+            </span>
+            <div className="md:flex md:flex-col md:gap-8 hidden">
+              {data?.lessons.map((lesson) => {
+                return (
+                  <Lesson
+                    key={lesson.id}
+                    title={lesson.title}
+                    slug={lesson.slug}
+                    availableAt={new Date(lesson.availableAt)}
+                    type={lesson.lessonType}
+                  />
+                );
+              })}
+            </div>
+          </>
+        )}
+      </aside>
+    </>
   );
 }
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -30,7 +30,7 @@ export function Video(props: VideoProps) {
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
-        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+        <div className="h-full w-full md:max-w-[1100px] max-w-[720px] max-h-[60vh] aspect-video">
           <Player>
             <Youtube videoId={data.lesson.videoId} />
             <DefaultUi />
