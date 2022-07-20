6abe16b37a3d2a41d1a504a170db3045a3fcae73
Italo Souza Melo <italosm29@gmail.com>
2022-07-20 13:53:49 -0300

    add state login on Subscribe page

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -12,6 +12,8 @@ export function Subscribe() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [passwordMatch, setPasswordMatch] = useState("");
+  const [isLogin, setIsLogin] = useState(true);
 
   const [createSubscriber, { loading }] = useCreateSubscriberMutation();
   const [publishSubscriber] = usePublishSubscriberMutation();
@@ -38,7 +40,7 @@ export function Subscribe() {
   return (
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
       <div className="w-full max-w-[1100px]  md:px-8 md:flex items-center justify-between mt-8 mx-auto">
-        <div className="max-w-[640px] ">
+        <div className="max-w-[640px]">
           <div className="flex mb-4 justify-center">
             <LogoIcon />
           </div>
@@ -53,48 +55,103 @@ export function Subscribe() {
             oportunidades do mercado.
           </p>
         </div>
-        <div className="p-6 mt-8  md:mt-0 bg-gray-700 border border-gray-500 rounded">
-          <strong className="text-2xl mb-6 block">
-            Inscreva-se gratuitamente
-          </strong>
-          <form
-            onSubmit={handleSubscriber}
-            className="flex flex-col gap-2 w-full"
-          >
-            <input
-              onChange={(event) => setName(event.target.value)}
-              className="bg-gray-900 rounded px-5 h-14"
-              type="text"
-              placeholder="Seu nome completo"
-            />
-            <input
-              onChange={(event) => setEmail(event.target.value)}
-              className="bg-gray-900 rounded px-5 h-14"
-              type="text"
-              placeholder="Digite seu e-mail"
-            />
-            <input
-              onChange={(event) => setPassword(event.target.value)}
-              className="bg-gray-900 rounded px-5 h-14"
-              type="password"
-              placeholder="Digite sua senha"
-            />
-            <input
-              onChange={(event) => setPassword(event.target.value)}
-              className="bg-gray-900 rounded px-5 h-14"
-              type="password"
-              placeholder="Confirme sua senha"
-            />
-            <button
-              type="submit"
-              disabled={loading}
-              className="mt-4 mb-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50"
+        {isLogin ? (
+          <div className="w-[340px] text-center p-6 mt-8  md:mt-0 bg-gray-700 border border-gray-500 rounded">
+            <strong className="text-2xl mb-6 block">
+              Inscreva-se gratuitamente
+            </strong>
+            <form
+              onSubmit={handleSubscriber}
+              className="flex flex-col gap-2 w-full"
             >
-              Garantir minha vaga
-            </button>
-            <button>Faça já seu login</button>
-          </form>
-        </div>
+              <input
+                onChange={(event) => setName(event.target.value)}
+                className="bg-gray-900 rounded px-5 h-14"
+                type="text"
+                placeholder="Seu nome completo"
+              />
+              <input
+                onChange={(event) => setEmail(event.target.value)}
+                className="bg-gray-900 rounded px-5 h-14"
+                type="text"
+                placeholder="Digite seu e-mail"
+              />
+              <input
+                onChange={(event) => setPassword(event.target.value)}
+                className="bg-gray-900 rounded px-5 h-14"
+                type="password"
+                placeholder="Digite sua senha"
+              />
+              <input
+                onChange={(event) => setPasswordMatch(event.target.value)}
+                className="bg-gray-900 rounded px-5 h-14"
+                type="password"
+                placeholder="Confirme sua senha"
+              />
+
+              <button
+                type="submit"
+                disabled={loading}
+                className="mt-4 mb-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50"
+              >
+                Garantir minha vaga
+              </button>
+              <button
+                className="hover:border hover:border-green-700 p-2"
+                onClick={() => {
+                  setIsLogin(!isLogin);
+                }}
+              >
+                Faça já seu login
+              </button>
+            </form>
+          </div>
+        ) : (
+          <div className="w-[340px] p-6 mt-8  md:mt-0 bg-gray-700 border border-gray-500 rounded">
+            <strong className="text-2xl mb-6 block text-center">
+              Acessar plataforma
+            </strong>
+            <form
+              onSubmit={handleSubscriber}
+              className="flex flex-col gap-2 w-full"
+            >
+              <input
+                onChange={(event) => setEmail(event.target.value)}
+                className="bg-gray-900 rounded px-5 h-14"
+                type="text"
+                placeholder="Digite seu e-mail"
+              />
+              <input
+                onChange={(event) => setPassword(event.target.value)}
+                className="bg-gray-900 rounded px-5 h-14"
+                type="password"
+                placeholder="Digite sua senha"
+              />
+
+              <button
+                type="submit"
+                disabled={loading}
+                className="mt-4 mb-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50"
+              >
+                Acessar Plataforma
+              </button>
+              <button
+                className="hover:border hover:border-green-700 p-2"
+                onClick={() => {
+                  setIsLogin(!isLogin);
+                }}
+              >
+                Inscreva-se já
+              </button>
+              <a
+                href="#"
+                className="text-center hover:border hover:border-green-700 p-2"
+              >
+                Esqueceu a senha?
+              </a>
+            </form>
+          </div>
+        )}
       </div>
       <img
         src="https://res.cloudinary.com/drgpo0mge/image/upload/v1657859075/Group_7735_aeua26.png"
