062e2419c1966efdae6591c6ed2ec47670f8fbd9
Italo Souza Melo <italosm29@gmail.com>
2022-07-20 12:25:06 -0300

    Ajustes na Subscribe page

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -37,23 +37,23 @@ export function Subscribe() {
 
   return (
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-      <div className="w-full max-w-[1100px] px-16 md:px-8 md:flex items-center justify-between mt-20 mx-auto">
+      <div className="w-full max-w-[1100px]  md:px-8 md:flex items-center justify-between mt-8 mx-auto">
         <div className="max-w-[640px] ">
           <div className="md:block flex justify-center">
             <LogoIcon />
           </div>
-          <h1 className="mt-8 text-[2.5rem] leading-tight">
+          <h1 className="p-4 mt-2 text-[2.5rem] leading-tight">
             Construa uma{" "}
             <strong className="text-blue-500">aplicação completa</strong>, do
-            zero, com <strong className="text-blue-500">React</strong>
+            zero, com <strong className="text-blue-500">React</strong>.
           </h1>
-          <p className="mt-4 text-gray-200 leading-relaxed">
+          <p className="px-4 text-gray-200 leading-relaxed">
             Em apenas uma semana você vai dominar na prática uma das tecnologias
             mais utilizadas e com alta demanda para acessar as melhores
             oportunidades do mercado.
           </p>
         </div>
-        <div className="p-8 mt-8 md:mt-0 bg-gray-700 border border-gray-500 rounded">
+        <div className="p-6 mt-8  md:mt-0 bg-gray-700 border border-gray-500 rounded">
           <strong className="text-2xl mb-6 block">
             Inscreva-se gratuitamente
           </strong>
